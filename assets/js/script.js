e08454643a4ac11870a6a400944b83ce51ee13ec
reececorkhill <reece@smartit-webdevelopment.com>
2024-01-27 01:31:39 +0000

    Removed console log and added comments.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,65 @@
 $(document).ready(function() {
 
-    function displayForecasts () {
+    // Function to display the weather today based on the city the user searches for.
+    function displayWeatherToday () {
 
-        var cityName = $("#search-input").val().trim();
-        var apiKey = "a9c66b40cb4e146f6ec43b344359e309";
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&mode=json&units=metric&appid=" + apiKey;
+        var cityName = $("#search-input").val().trim();                                                                                             // Taking user input value and removing whitespace.
+        var apiKey = "a9c66b40cb4e146f6ec43b344359e309";                                                                                            // OpenWeatherMap API Key.
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&mode=json&units=metric&appid=" + apiKey;                 // Setting the query URL following API docs spec and specifying metric units of measure.
 
-        fetch(queryURL)
-        .then(function (response) {
-            return response.json();
+        fetch(queryURL)                                                                                                                             // Performing a fetch call on the queryURL.
+        .then(function (response) {                                                                                                                 // Convering the response to JSON.
+            return response.json();                                                                                                                 
         })
-        .then(function (data) {
-            // console.log(data);
-            var cityName = (data.name);
-            var forecastDate = dayjs.unix(data.dt).format("DD-MM-YYYY");
-            var forecastIcon = (data.weather[0].icon);
-            var forcastIconLink = ("https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png")
-            var forecastTemp = (data.main.temp.toFixed(1));
-            var forecastWind = (data.wind.speed.toFixed(0));
-            var forecastHumidity = (data.main.humidity);
+        .then(function (data) {                                                                                                                     // Accessing the returned data after being converted.
 
-            var todaysForecastDiv = $("<div>");
-            todaysForecastDiv.css({"border":"solid black 1px"})
+            var cityName = (data.name);                                                                                                             // Setting the cityName variable to the name from data.
+            var forecastDate = dayjs.unix(data.dt).format("DD-MM-YYYY");                                                                            // Setting the forcastDate variable to the unix timestamp from data and formatting it using dayJS.
+            var forecastIcon = (data.weather[0].icon);                                                                                              // Setting the forcastIcon variable to the icon code from data.
+            var forcastIconLink = ("https://openweathermap.org/img/wn/" + forecastIcon + "@2x.png");                                                // Setting the forecastIconLink variable to the docs' icon search link using the forecastIcon code from data.
+            var forecastTemp = (data.main.temp.toFixed(1));                                                                                         // Setting the forecastTemp variable to the temp from data and specifying it to 1 decimal place.
+            var forecastWind = (data.wind.speed.toFixed(0));                                                                                        // Setting the forecastWind variable to the wind from data and specifying it to 0 decimal places.
+            var forecastHumidity = (data.main.humidity);                                                                                            // Setting the forecastHumidity variable to the humidity from data.
+
+            var todaysForecastDiv = $("<div>");                                                                                                     // Creating a div for today's weather information.
+            todaysForecastDiv.css({"border":"solid black 1px"});                                                                                    // Adding css border to the div.
 
             // City Name
-            var todaysForecastCity = $("<h2>");
-            todaysForecastCity.text(cityName);
-            todaysForecastDiv.append(todaysForecastCity);
+            var todaysForecastCity = $("<h2>");                                                                                                     // Creating a h2 element for the city name.
+            todaysForecastCity.text(cityName);                                                                                                      // Setting the h2 text to the show the cityName.
+            todaysForecastDiv.append(todaysForecastCity);                                                                                           // Appending the h2 element to todaysForecastDiv.
 
             // Today's Date
-            var todaysDate = $("<p>");
-            todaysDate.text("(" + forecastDate + ")");
-            todaysForecastDiv.append(todaysDate);
+            var todaysDate = $("<p>");                                                                                                              // Creating a p element for today's date.
+            todaysDate.text("(" + forecastDate + ")");                                                                                              // Setting the p text to show forecastDate.
+            todaysForecastDiv.append(todaysDate);                                                                                                   // Appending the p element to todaysForecastDiv.
 
             // Weather Forecast Icon
-            var todaysForecastIcon = $(`<img src="${forcastIconLink}">`)
-            todaysForecastDiv.append(todaysForecastIcon);
+            var todaysForecastIcon = $(`<img src="${forcastIconLink}">`);                                                                           // Creating an img element which includes the icon link in source.
+            todaysForecastDiv.append(todaysForecastIcon);                                                                                           // Appending the img element to todaysForecastDiv.
 
             // Temperature Forecast
-            var todaysForecastTemp = $("<p>");
-            todaysForecastTemp.text("Temp: " + forecastTemp + " °C");
-            todaysForecastDiv.append(todaysForecastTemp);
+            var todaysForecastTemp = $("<p>");                                                                                                      // Creating a p element for the temp value.
+            todaysForecastTemp.text("Temp: " + forecastTemp + " °C");                                                                               // Setting the p text to show the temp with degrees celsius added on.
+            todaysForecastDiv.append(todaysForecastTemp);                                                                                           // Appending the p element to todaysForecastDiv.
 
             // Wind Forecast
-            var todaysForecastWind = $("<p>");
-            todaysForecastWind.text("Wind: " + forecastWind + " /mph")
-            todaysForecastDiv.append(todaysForecastWind);
+            var todaysForecastWind = $("<p>");                                                                                                      // Creating a p element for the wind value.
+            todaysForecastWind.text("Wind: " + forecastWind + " /mph");                                                                             // Setting the p text to show the wind with miles per hour added on.
+            todaysForecastDiv.append(todaysForecastWind);                                                                                           // Appending the p element to todaysForecastDiv.
 
             // Humidity Forecast
-            var todaysForecastHumidity = $("<p>");
-            todaysForecastHumidity.text("Humidity: " + forecastHumidity + "%")
-            todaysForecastDiv.append(todaysForecastHumidity);
+            var todaysForecastHumidity = $("<p>");                                                                                                  // Creating a p element for the humidity value.
+            todaysForecastHumidity.text("Humidity: " + forecastHumidity + "%");                                                                     // Setting the p text to show the humidity with % added on.
+            todaysForecastDiv.append(todaysForecastHumidity);                                                                                       // Appending the p element to todaysForecastDiv.
 
-            $("#today").append(todaysForecastDiv);
+            $("#today").append(todaysForecastDiv);                                                                                                  // Appending the todaysForecastDiv to the #today section.
         });
 
     }
 
-    // This function handles events search button is clicked
-    $("#search-button").on("click", function (event) {
-        event.preventDefault();
-        displayForecasts();
+    $("#search-button").on("click", function (event) {                                                                                              // Function to handle events when search button is clicked.
+        event.preventDefault();                                                                                                                     // Preventing default behaviour.
+        displayWeatherToday();                                                                                                                      // Calling the displayWeatherToday function.
     });
 });
